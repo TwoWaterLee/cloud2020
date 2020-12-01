9720bf9214880ee83e87c2789c32188bd78794f0
liershui <liershui@navercorp.com>
2020-12-01 19:37:51 +0800

    Finish eureka

--- cloud-provider-payment8001/src/main/java/com/naver/cloud2020/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/naver/cloud2020/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.naver.cloud2020.entity.Payment;
 import com.naver.cloud2020.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -18,6 +21,9 @@ public class PaymentController {
 
 	@Value("${server.port}")
 	private String port;
+
+	@Resource
+	private DiscoveryClient discoveryClient;
 
 	@PostMapping(value = "/payment/create")
 	public CommonResult create(@RequestBody Payment payment) {
@@ -39,5 +45,18 @@ public class PaymentController {
 		} else {
 			return new CommonResult(444, port + " query fail, ID: " + id, null);
 		}
+	}
+
+	@GetMapping(value = "/payment/discovery")
+	public Object discovery() {
+		List<String> services = discoveryClient.getServices();
+		for (String element : services) {
+			log.info("*** element: " + element);
+		}
+		List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+		for (ServiceInstance instance : instances) {
+			log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+		}
+		return this.discoveryClient;
 	}
 }
