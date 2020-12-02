ec2cc8b9e5934726e60d34457704c2e3401d39ff
liershui <liershui@navercorp.com>
2020-12-02 16:33:21 +0800

    Finish hystrix fallback

--- cloud-provider-hystrix-payment8001/src/main/java/com/naver/cloud2020/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/naver/cloud2020/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.naver.cloud2020.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -11,13 +13,22 @@ public class PaymentService {
 		return "线程池: " + Thread.currentThread().getName() + " paymentInfo_OK,id: " + id + "\t" + "哈哈哈";
 	}
 
+	@HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "4000")
+	})
 	public String paymentInfo_TimeOut(Integer id) {
-		int timeNumber = 3;
+
+//		int age = 10 / 0;
+		int timeNumber = 2;
 		try {
 			TimeUnit.SECONDS.sleep(timeNumber);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return "线程池: " + Thread.currentThread().getName() + " paymentInfo_TimeOut,id: " + id + "\t" + "呜呜呜" + " 耗时: " + timeNumber;
+	}
+
+	public String paymentInfo_TimeOutHandler(Integer id) {
+		return "线程池: " + Thread.currentThread().getName() + " 系统繁忙，请稍后再试, id: " + id + "\t" + "哭了";
 	}
 }
