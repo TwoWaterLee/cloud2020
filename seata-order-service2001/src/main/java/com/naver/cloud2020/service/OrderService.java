fdf04f1a2b523f95ca0dcd852360a959bb325a2d
liershui <liershui@navercorp.com>
2020-12-04 16:35:37 +0800

    Finish seata-order-service2001

---
+++ seata-order-service2001/src/main/java/com/naver/cloud2020/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.naver.cloud2020.service;
+
+import com.naver.cloud2020.domain.Order;
+
+public interface OrderService {
+	void create(Order order);
+}
---
+++ seata-order-service2001/src/main/java/com/naver/cloud2020/service/StorageService.java
@@ -0,0 +1,13 @@
+package com.naver.cloud2020.service;
+
+import com.naver.cloud2020.domain.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "seata-storage-service")
+public interface StorageService {
+
+	@PostMapping(value = "/storage/decrease")
+	CommonResult decrease(@RequestParam("productId") Long productId, @RequestParam("count") Integer count);
+}
